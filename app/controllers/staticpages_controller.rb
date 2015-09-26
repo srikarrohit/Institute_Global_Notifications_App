237920b74335995364619d2552c46196f2a0e68a
srikarrohit <srikarrohit@gmail.com>
2015-09-26 15:55:52 +0530

    navbar changed

--- app/controllers/staticpages_controller.rb
+++ app/controllers/staticpages_controller.rb
@@ -3,17 +3,13 @@ class StaticpagesController < ApplicationController
   def home
 		@posts=Post.all
 		file=File.read("public/users.json")
-    data_hash = JSON.parse(file)
+    	data_hash = JSON.parse(file)
 		students = data_hash["students"]
 		students.each do |data|
 			if data["id"] == getid
 				@notifs=data["notifids"]
 			end
 		end
-	#	respond_to do |format|
-  #  	format.html
-  #  	format.json
-  #	end
 		if logged_in?
 			@usercat= Usercat.find_by(user_id: getid)
 		else
@@ -26,22 +22,29 @@ class StaticpagesController < ApplicationController
 	end
 	def notif
 		notifid = params[:notif]
+		@notifid=notifid
 		file=File.read("public/users.json")
 		data_hash = JSON.parse(file)
 		f=File.open("public/users.json","w")
 		students=data_hash["students"]
-		students.each do |data|
-			if data["id"] == getid
-				notifs = data["notifids"].split(',')
-    		notifs.map!{ |n| n.to_i }
-				notifs.delete(notifid.to_i)
-				notifs=notifs.join(',')
-				data["notifids"]=notifs
+		#respond_to do |format|
+					students.each do |data|
+						if data["id"] == getid
+							notifs = data["notifids"].split(',')
+		    				notifs.map!{ |n| n.to_i }
+							notifs.delete(notifid.to_i)
+							notifs=notifs.join(',')
+							data["notifids"]=notifs
+						end
+					end
+			data_hash["students"]=students
+			data_hash=data_hash.to_json
+			if f.write(data_hash)
+				respond_to do |format|
+				format.js
+				end
 			end
-		end
-		data_hash["students"]=students
-		data_hash=data_hash.to_json
-		f.write(data_hash)
-		redirect_to root_path
+    		
+  		#end
 	end
 end
